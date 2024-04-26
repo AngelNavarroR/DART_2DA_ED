6852231179aa5bcc388929bb28275a067198c055
navarroangelr <navarroangelr@gmail.com>
2024-04-25 21:23:53 -0500

    Clase

---
+++ 1.hola_mundo/ejercicios/AngelNavarro/hola_mundo.dart
@@ -0,0 +1,4 @@
+main(){
+    print("    Ejemplo de hola mundo!!!!!!!!!!!!");
+    print("====Ejemplo de hola mundo!!!!!!!!!!!!");
+}
---
+++ 2.variables/ejercicios/AngelNavarro/variables.dart
@@ -0,0 +1,17 @@
+main(){
+  var velocidad = 20;
+  var pi = 3.14;
+  var resultado = (velocidad * pi);
+  print("    " + resultado.toString());
+  // Ejemplo de for loop
+  // Const se asigan su espacio  en el momento de compilacion
+  const collection = {1,2,3,4,5,6};
+  for (var element in collection) {
+     print("    " + (element * pi).toString());
+  }
+  // Final reserva cuando se ejecuta
+
+  // late
+  late String fecha;
+
+}
